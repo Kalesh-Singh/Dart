7de6e8742e2f7e74efc96d168c8a77bbcc704a2a
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-10-19 15:38:14 -0400

    02. Printing

---
+++ 02-printing.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = myName();
+  name.length;
+  print ('My name is $name');
+  print ('The length of my name is ${name.length}');
+}
+
+String myName() {
+  return 'Kalesh';
+}
