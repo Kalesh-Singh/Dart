24a3177befb9d8e1ef89b21b645c89608d31aa9f
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-10-20 17:37:35 -0400

    06. Json

---
+++ 06-json.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+
+void main() {
+  var rawJson = '{"url": "http://blah.jpg", "id": 1}';
+  var parsedJson = json.decode(rawJson);
+  print(parsedJson);
+
+  print(parsedJson['url']);
+  print(parsedJson['id']);
+}
