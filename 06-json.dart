e6ca08b70f59c16334e663eb9f52996a98e178b7
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-10-20 23:37:45 -0400

    Model Instances and Named Constructors

--- 06-json.dart
+++ 06-json.dart
@@ -7,4 +7,26 @@ void main() {
 
   print(parsedJson['url']);
   print(parsedJson['id']);
+
+  var imageModel = ImageModel(parsedJson['id'], parsedJson['url']);
+  print(imageModel.id);
+  print(imageModel.url);
+
+  // Using a named constructor
+  imageModel =  ImageModel.fromJson(parsedJson);
+  print(imageModel.id);
+  print(imageModel.url);
+}
+
+class ImageModel {
+  int id;
+  String url;
+
+  ImageModel(this.id, this.url);
+
+  // Named Constructor
+  ImageModel.fromJson(parsedJson) {
+    id = parsedJson['id'];
+    url = parsedJson['url'];
+  }
 }
